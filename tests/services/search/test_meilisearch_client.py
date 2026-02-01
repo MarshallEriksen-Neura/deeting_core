a101ae7dc2ac808b6fa8645837fcae4f9610f059
NeuralCore <admin@ethereals.space>
2026-02-01 09:22:33 +0000

    test: cover meilisearch configured case

--- tests/services/search/test_meilisearch_client.py
+++ tests/services/search/test_meilisearch_client.py
@@ -1,5 +1,3 @@
-import pytest
-
 from app.meilisearch_client import meilisearch_is_configured
 
 
@@ -7,3 +5,9 @@ def test_meilisearch_is_configured_false_when_missing_url(settings):
     settings.MEILISEARCH_URL = ""
     settings.MEILISEARCH_API_KEY = ""
     assert meilisearch_is_configured() is False
+
+
+def test_meilisearch_is_configured_true_when_url_set(settings):
+    settings.MEILISEARCH_URL = "http://localhost:7700"
+    settings.MEILISEARCH_API_KEY = ""
+    assert meilisearch_is_configured() is True
